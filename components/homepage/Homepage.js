e50445e13ad89c2a2cbdcb71ac48ac333ce57bf1
Sriniwas <sriniwasx@gmail.com>
2019-11-02 14:32:13 +0530

    Changed the headerbar and added made the search can fetch data now

--- components/homepage/Homepage.js
+++ components/homepage/Homepage.js
@@ -6,7 +6,7 @@ import Section from './Section'
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
 class Homepage extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = {
       search: "",
@@ -15,19 +15,24 @@ class Homepage extends Component {
       bestSellers: []
     }
   }
-  
+
   static navigationOptions = {
-    title: 'Book Reviews',
-    headerStyle: {
-        backgroundColor: "#292929"
-    },
-    headerTintColor: "#fff",
-    headerRight: (
-        <TouchableOpacity onPress={()=> this.setState({ showSearch: !this.state.showSearch })} style={{ paddingHorizontal: 10 }}>
-            <Icon name='search' color="#ffffff"/>
-        </TouchableOpacity>
-      ),
-   };
+    // title: 'Book Reviews',
+    // headerStyle: {
+    //     backgroundColor: "#292929"
+    // },
+    // headerTintColor: "#fff",
+    // headerRight: (
+    //     <TouchableOpacity onPress={ this.toggleSearchBox } style={{ paddingHorizontal: 10 }}>
+    //         <Icon name='search' color="#ffffff"/>
+    //     </TouchableOpacity>
+    //   ),
+    header: null
+  };
+
+  toggleSearchBox = () => {
+    this.setState({ showSearch: true })
+  }
 
   bookCategories = [
     {
@@ -63,7 +68,7 @@ class Homepage extends Component {
   searchBooks = (query) => {
     api.searchBooks(query).then((resp) => {
       this.setState({ mostRecent: resp.data.items })
-    })
+    }).catch(err => console.log("Search Error ", err))
   }
 
   styles = StyleSheet.create({
@@ -83,26 +88,31 @@ class Homepage extends Component {
   render() {
     let searchbar;
     const { search, showSearch, bestSellers } = this.state;
-
-    if(showSearch){
-      searchbar = <SearchBar placeholder="Type Here..." onChange={this.updateSearch} value={search} platform="android" />
+    console.log("OBIDF ", this.state.mostRecent)
+    if (showSearch) {
+      searchbar = <SearchBar placeholder="Search..." onChangeText={this.updateSearch} value={search} platform="android" />
     }
+
     return (
       <View>
-        {/* <Header
+        <Header
           leftComponent={{ icon: 'menu', color: '#fff' }}
           centerComponent={{ text: 'Book Reviews', style: { color: '#fff' } }}
-          rightComponent={{ icon: 'search', color: '#fff', onPress: ()=>{ this.setState({ showSearch: !showSearch }) } }}
+          rightComponent={{ icon: 'search', color: '#fff', onPress: () => { this.setState({ showSearch: !showSearch }) } }}
           containerStyle={{ backgroundColor: "black" }}
-        /> */}
-        {searchbar}
-        <ScrollView>
-        {this.bookCategories.map((cat)=> {
-            return (<Section navigation={this.props.navigation} key={cat.cat_id} displayName={cat.title} type={cat.id} />)
-        })}
-        </ScrollView>        
+        />
 
-    </View>
+        {searchbar}
+
+
+
+        <ScrollView>
+          {this.bookCategories.map((cat) => {
+            return (<Section navigation={this.props.navigation} key={cat.cat_id} displayName={cat.title} type={cat.id} />)
+          })}
+        </ScrollView>
+
+      </View>
     )
   }
 }
